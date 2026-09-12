6150e185663704d7a8a0b3acb5a3ba83ca99ddbd
Caio Magno <caiomagno560@gmail.com>
2026-09-12 14:23:37 -0400

    Migracao para Spring Boot e Thymeleaf

---
+++ src/main/java/com/autoparts/sitepecascarro/SitepecascarroApplication.java
@@ -0,0 +1,13 @@
+package com.autoparts.sitepecascarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitepecascarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitepecascarroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoparts/sitepecascarro/SitepecascarroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoparts.sitepecascarro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitepecascarroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
