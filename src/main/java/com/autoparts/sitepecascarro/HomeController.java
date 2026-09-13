2a96167c6ff8f87b56536f4162eec669ef29ff3c
Caio Magno <caiomagno560@gmail.com>
2026-09-12 20:19:25 -0400

    Conecta home com camada de pecas

--- src/main/java/com/autoparts/sitepecascarro/HomeController.java
+++ src/main/java/com/autoparts/sitepecascarro/HomeController.java
@@ -1,4 +1,6 @@
-package com.autoparts.sitepecascarro;
+package com.autoparts.sitepecascarro.controller;
+
+import com.autoparts.sitepecascarro.service.PecaService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,18 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    private final PecaService pecaService;
+
+    public HomeController(PecaService pecaService) {
+        this.pecaService = pecaService;
+    }
+
     @GetMapping("/")
     public String home(Model model) {
 
         model.addAttribute("tituloPagina", "AutoParts - Peças Automotivas");
         model.addAttribute("tituloPrincipal", "ENCONTRE A PEÇA");
+        model.addAttribute("pecas", pecaService.listarTodas());
 
         return "index";
-    }
-
-    @GetMapping("/lista")
-    public String lista(Model model) {
-
-        return "pecas/lista";
     }
 }
